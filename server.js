b054733d167c583328012f660ccc9b0f1c68e7b4
rorshrak <tarunghelot@gmail.com>
2016-09-29 20:51:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -32,6 +32,8 @@ function createTemplate(data){
     var htmlTemplate=`
     <html>
       <head><title>${title}</title>
+        <link href="/ui/style.css" rel="stylesheet" />
+        <meta name="viewport",content="width=device-width,initial-scale=1"></meta>
       </head>
       <body>
         <h1>${heading}</h1>
