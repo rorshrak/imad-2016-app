770b0fa35bd6c68892a5712a36ce7018b766bfd6
rorshrak <tarunghelot@gmail.com>
2016-09-29 20:24:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var articles={
+var articles = {
     'article-one':{title:'article-one|Tarun Gehlot',
                   heading:'Article One',
                   date:'29-09-2016',
@@ -48,17 +48,17 @@ app.get('/',function(req,res){
    res.sendFile(path.join(__dirname,'ui','index.html'));
 });
 
-app.get('/ui/style.css', function (req, res) {
+app.get('/ui/style.css',function(req,res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
+app.get('/ui/madi.png',function(req,res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
 app.get('/:articleName',function(req,res){
-    var articleName=req.params.articleName;
-  res.send(createTemplate(articles[articleName]));  
+    var articleName = req.params.articleName;
+    res.send(createTemplate(articles[articleName]));  
 });
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
