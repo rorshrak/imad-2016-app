b7a226de5945662ca81a97ff570b62cd7a7abaef
rorshrak <tarunghelot@gmail.com>
2016-10-04 20:17:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ function createTemplate(data){
     return htmlTemplate;    
 }
 
-app.get('/', function (req, res) {
+app.get('/ui', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
