bf23e11217934b2a628099cfc067dab789a99947
rorshrak <tarunghelot@gmail.com>
2016-10-08 13:19:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -63,6 +63,11 @@ app.get('/ui/madi.png',function(req,res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+app.get('/ui/main.js',function(req,res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
+
 app.get('/:articleName',function(req,res){
     var articleName = req.params.articleName;
     res.send(createTemplate(articles[articleName]));  
