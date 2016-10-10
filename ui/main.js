20ea81fe884a40e34d84a875dd1ffab48716e121
rorshrak <tarunghelot@gmail.com>
2016-10-10 16:10:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,3 @@
-console.log('Loaded!');
-var element = document.getElementById('image');
-
-var left=0;
-function move()
-{
-    left=left+5;
-    element.style.marginLeft=left+'px';
-}
-
-element.onclick = function(){
-    var interval = setInterval(move,50);
-};
 
 //to display the comments
 var commentget=document.getElementById('comment');
