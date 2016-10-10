d6bfaaf4823fb3be964a0ca3c13dcb936e74ef16
rorshrak <tarunghelot@gmail.com>
2016-10-10 15:33:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,3 +11,35 @@ function move()
 element.onclick = function(){
     var interval = setInterval(move,50);
 };
+
+//to display the comments
+
+var commentInput=document.getElementById('comment');
+var comment=commentInput.value;
+var submit=document.getElementById('submit');
+
+submit.onclick=function()
+{
+  var request=new XMLHttpRequest();
+  request.onreadystatechange =  function()
+   {
+       if(request.readyState===XMLHttpRequest.DONE)
+       {
+           if(request.status==200)
+           {
+             var comments=request.responseText;
+             comments=JSON.parse(comments);
+             var list='';
+             for(var i=0; i<comments.length; i++)
+             {
+                 list+='<li>' + comments[i] + '</li>';
+             }
+             var ul=document.getElementById('ul');
+             ul.innerHtml=list;
+           }
+       }
+   };
+};
+
+request.open('GET','http://rorshrak.imad.hasura-app.io/submit-comment?comment='+comment,true);
+request.send('null');
